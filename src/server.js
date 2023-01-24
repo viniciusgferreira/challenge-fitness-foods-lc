938235d0dbc40446b878de19f06a3a151838e877
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-24 12:16:01 -0300

    refactor mongodb connection

--- src/server.js
+++ src/server.js
@@ -4,29 +4,27 @@ import { router } from './routes/router.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const MONGODB = process.env.MONGODB;
+const MONGOHOST = process.env.MONGOHOST;
 const MONGOUSER = process.env.MONGOUSER;
 const MONGOPASS = process.env.MONGOPASS;
-const uri = `mongodb+srv://${MONGOUSER}:${MONGOPASS}@${MONGODB}`;
-export const mongodbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-try {
-  await mongodbClient.connect();
-  console.log('mongodb connected');
-
-  // START SERVER
-  const PORT = process.env.PORT || 3000;
-  const app = express();
-
-  //JSON PARSER
-  app.use(express.json());
-
-  // ROUTER
-  app.use('/v1', router);
-
-  app.listen(PORT, () => console.log(`API Fitness Foods Products is running on port http://localhost:${PORT}`));
+const MONGODB = process.env.MONGODB;
+const mongoURI = `mongodb+srv://${MONGOUSER}:${MONGOPASS}@${MONGOHOST}`;
+export const mongodbClient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-} catch (error) {
-  console.error(error.message);
-}
+await mongodbClient.connect();
+console.log('mongodb connected');
+const db = mongodbClient.db(MONGODB);
+export const collectionProducts = db.collection('products');
+
+// START SERVER
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//JSON PARSER
+app.use(express.json());
+
+// ROUTER
+app.use('/v1', router);
+
+app.listen(PORT, () => console.log(`API Fitness Foods Products is running on port http://localhost:${PORT}`));
