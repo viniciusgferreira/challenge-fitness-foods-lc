3338ee034df55aad20a009651f1d4a034302578a
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-25 22:18:02 -0300

    model for each product

---
+++ src/api/models/createProductObj.js
@@ -0,0 +1,32 @@
+export async function createProductObj(product) {
+  return new Promise((resolve) => {
+    const productObj = {
+      'code': product.code,
+      'status': product.status,
+      'imported_t': product.imported_t,
+      'url': product.url,
+      'creator': product.creator,
+      'created_t': product.created_t,
+      'last_modified_t': product.last_modified_t,
+      'product_name': product.product_name,
+      'quantity': product.quantity,
+      'brands': product.brands,
+      'categories': product.categories,
+      'labels': product.labels,
+      'cities': product.cities,
+      'purchase_places': product.purchase_places,
+      'stores': product.stores,
+      'ingredients_text': product.ingredients_text,
+      'traces': product.traces,
+      'serving_size': product.serving_size,
+      'serving_quantity': product.serving_quantity,
+      'nutriscore_score': product.nutriscore_score,
+      'nutriscore_grade': product.nutriscore_grade,
+      'main_category': product.main_category,
+      'image_url': product.image_url
+    };
+
+    return resolve(productObj);
+
+  });
+}
