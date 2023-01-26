173075922c595cf5942ebbee6d2f18aaae2f5455
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-26 14:52:33 -0300

    set mongo db events listeners

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { router } from './api/v1/routes/router.js';
 import * as dotenv from 'dotenv';
+import { ConfigApiStatus } from './api/v1/services/getApiStatus.js';
+
 dotenv.config();
 
 const MONGOHOST = process.env.MONGOHOST;
@@ -10,12 +12,13 @@ const MONGOPASS = process.env.MONGOPASS;
 const MONGODB = process.env.MONGODB;
 const mongoURI = `mongodb+srv://${MONGOUSER}:${MONGOPASS}@${MONGOHOST}`;
 export const mongodbClient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ConfigApiStatus();
 
-
-await mongodbClient.connect();
-console.log('mongodb connected');
-const db = mongodbClient.db(MONGODB);
-export const collectionProducts = db.collection('products');
+// CONNECT TO MONGODB
+export const mongodbConnection = await mongodbClient.connect();
+export const database = mongodbClient.db(MONGODB);
+export const collectionProducts = database.collection('products');
+export const collectionImport = database.collection('import');
 
 // START SERVER
 const PORT = process.env.PORT || 3000;
@@ -27,4 +30,4 @@ app.use(express.json());
 // ROUTER
 app.use('/v1', router);
 
-app.listen(PORT, () => console.log(`API Fitness Foods is running on port http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`API Fitness Foods is running on http://localhost:${PORT}`));
