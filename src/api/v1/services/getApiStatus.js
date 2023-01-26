89562de11f12c6aabdb25e0af600b8a34cc82398
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-26 14:52:16 -0300

    roote route with uptime and mem usage

---
+++ src/api/v1/services/getApiStatus.js
@@ -0,0 +1,29 @@
+import { mongodbClient } from '../../../server.js';
+
+// SAVE CONNECTION STATUS (isConnected not available anymore)
+let isConnected;
+export function ConfigApiStatus() {
+
+  mongodbClient.on('connectionCreated', () => {
+    console.log('mongodb connected');
+    isConnected = true;
+  });
+
+  mongodbClient.on('connectionClosed', () => {
+    console.log('mongodb disconnected');
+    isConnected = false;
+  });
+}
+
+export async function getApiStatus() {
+
+  const hello = await mongodbClient.db('admin').command({ hello: 1 });
+  const apiStatus = {
+    'uptimeSeconds': process.uptime(),
+    'memoryUsageBytes': process.memoryUsage(),
+    'mongodbConnection': isConnected,
+    'mongoIsWritable': hello.isWritablePrimary,
+  };
+  return apiStatus;
+}
+
