812a19c8e790ac49e16e3a082e876dc5262ed489
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-26 16:51:42 -0300

    new mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,24 +1,11 @@
 import express from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
 import { router } from './api/v1/routes/router.js';
 import * as dotenv from 'dotenv';
 import { ConfigApiStatus } from './api/v1/services/getApiStatus.js';
+import importProducts from './utils/importProducts.js';
 
 dotenv.config();
-
-const MONGOHOST = process.env.MONGOHOST;
-const MONGOUSER = process.env.MONGOUSER;
-const MONGOPASS = process.env.MONGOPASS;
-const MONGODB = process.env.MONGODB;
-const mongoURI = `mongodb+srv://${MONGOUSER}:${MONGOPASS}@${MONGOHOST}`;
-export const mongodbClient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 ConfigApiStatus();
-
-// CONNECT TO MONGODB
-export const mongodbConnection = await mongodbClient.connect();
-export const database = mongodbClient.db(MONGODB);
-export const collectionProducts = database.collection('products');
-export const collectionImport = database.collection('import');
 
 // START SERVER
 const PORT = process.env.PORT || 3000;
@@ -31,3 +18,6 @@ app.use(express.json());
 app.use('/v1', router);
 
 app.listen(PORT, () => console.log(`API Fitness Foods is running on http://localhost:${PORT}`));
+
+// CREATE CRON JOB
+//importProducts();
