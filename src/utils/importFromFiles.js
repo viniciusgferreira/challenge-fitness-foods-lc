1ed02ebfdf1f35745578707ec2c6b7bcfa42f1cf
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-27 08:37:08 -0300

    fix: count error

--- src/utils/importFromFiles.js
+++ src/utils/importFromFiles.js
@@ -2,14 +2,21 @@ import { mongodbClient } from './mongodbUtil.js';
 
 export async function importFromFiles(productsToImport) {
   const mongodbUtilConnection = await mongodbClient.connect();
-
+  console.log('importing products to database...');
   try {
     // IMPORT EACH PRODUCT
-    console.log(productsToImport[0].code);
-    const result = await mongodbUtilConnection.db('fitnessfoodsdb').collection('products').insertMany(productsToImport);
-    console.log('Inserted documents: ' + result.insertedCount);
+    let importedCount = 0;
+    for (let i = 0; i < productsToImport.length; i++) {
+      const query = { code: parseInt(productsToImport[i].code) };
+      const result = await mongodbUtilConnection.db('fitnessfoodsdb').collection('products').replaceOne(query, productsToImport[i], { upsert: true });
+      if (result.modifiedCount == 1) {
+        importedCount++;
+      }
+    }
+
+    console.log('products sucessfully imported: ' + importedCount);
     mongodbUtilConnection.close();
-    return result;
+    return importedCount;
   } catch (error) {
     return error;
   }
