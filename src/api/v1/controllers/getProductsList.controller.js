86b105dddd62357c6f13ba2c4a4440669c6a05bd
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-27 10:39:55 -0300

    Request pagination default limit 10

--- src/api/v1/controllers/getProductsList.controller.js
+++ src/api/v1/controllers/getProductsList.controller.js
@@ -2,7 +2,9 @@ import { getProductsList } from '../services/getProductsList.js';
 
 
 export async function controller_getProductsList(req, res) {
-  const products = await getProductsList();
+  const limit = parseInt(req.query.limit) || 10;
+  const page = parseInt(req.query.page) || 1;
+  const products = await getProductsList(limit, page);
   if (products.length > 0) {
     res.json(products);
   } else {
--- src/api/v1/services/getProductsList.js
+++ src/api/v1/services/getProductsList.js
@@ -1,9 +1,9 @@
 import { database } from '../../../server.js';
 
-export const getProductsList = async () => {
+export const getProductsList = async (limit, page) => {
 
   try {
-    return await database.collection('products').find().toArray();
+    return await database.collection('products').find().limit(limit).skip((page - 1) * limit).toArray();
   } catch (err) {
     console.log(err);
     return null;
