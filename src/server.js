13e680600e69d08deee1267e8a3d5035cdcae025
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-27 06:28:19 -0300

    mongodb server connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { router } from './api/v1/routes/router.js';
 import * as dotenv from 'dotenv';
-import { ConfigApiStatus } from './api/v1/services/getApiStatus.js';
 import importProducts from './utils/importProducts.js';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 dotenv.config();
-ConfigApiStatus();
+
+// CONNECT TO MONGODB
+const mongoURI = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOHOST}`;
+export const mongodbClient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+export const mongodbServerConnection = await mongodbClient.connect();
+export const database = mongodbClient.db(process.env.MONGODB);
 
 // START SERVER
 const PORT = process.env.PORT || 3000;
@@ -19,5 +24,3 @@ app.use('/v1', router);
 
 app.listen(PORT, () => console.log(`API Fitness Foods is running on http://localhost:${PORT}`));
 
-// CREATE CRON JOB
-//importProducts();
