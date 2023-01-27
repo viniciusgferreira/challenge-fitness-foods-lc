5a6dcf113e5ac04337cb14470ee7a65562a1aa5c
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-27 12:59:48 -0300

    fix: product already in trash

--- src/api/v1/controllers/deleteProduct.controller.js
+++ src/api/v1/controllers/deleteProduct.controller.js
@@ -3,11 +3,16 @@ import { setProduct } from '../services/setProduct.js';
 
 export async function controller_deleteProduct(req, res) {
   let { code } = req.params;
+  code = parseInt(code);
   const product = await getProduct(code);
   if (product) {
-    product.status = 'trash';
-    const editedProduct = await setProduct(code, product);
-    editedProduct.modifiedCount ? res.status(201).json({ 'msg': 'product sent to trash' }) : res.status(404).json({ 'msg': 'product not found' });
+    if (product.status == 'trash') {
+      res.status(406).json({ 'msg': 'product already in trash' });
+    } else {
+      product.status = 'trash';
+      const editedProduct = await setProduct(code, product);
+      editedProduct.modifiedCount ? res.status(201).json({ 'msg': 'product sent to trash' }) : res.status(404).json({ 'msg': 'product not found' });
+    }
   } else {
     res.status(404).json({ 'msg': 'product not found' });
   }
