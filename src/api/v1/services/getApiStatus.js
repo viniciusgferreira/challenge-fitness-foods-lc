d79b0ec0e8113783a0bf02c54153f9b992ba23f5
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-01-27 09:33:03 -0300

    get last import info from db

--- src/api/v1/services/getApiStatus.js
+++ src/api/v1/services/getApiStatus.js
@@ -3,13 +3,15 @@ import { mongodbServerConnection } from '../../../server.js';
 export async function getApiStatus() {
 
   const hello = await mongodbServerConnection.db('admin').command({ hello: 1 });
+  const last_import = await mongodbServerConnection.db('fitnessfoodsdb').collection('imports').find().sort({ imported_t: -1 }).limit(1).toArray();
   let connection = true;
   if (!hello) { connection = false; }
   const apiStatus = {
     'uptimeSeconds': process.uptime(),
-    'memoryUsageBytes': process.memoryUsage(),
     'mongodbConnection': connection,
     'mongoIsWritable': hello.isWritablePrimary,
+    'last_import': last_import,
+    'memoryUsageBytes': process.memoryUsage()
   };
   return apiStatus;
 }
